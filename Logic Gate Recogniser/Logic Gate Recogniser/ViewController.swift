a6cff35234c2814007eb2d8dcef04c2a3faab97a
Sonnie Hiles <psysh2@nottingham.ac.uk>
2019-10-16 12:26:47 +0100

    Initial commit

---
+++ Logic Gate Recogniser/Logic Gate Recogniser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Logic Gate Recogniser
+//
+//  Created by Sonnie Hiles on 16/10/2019.
+//  Copyright © 2019 Sonnie Hiles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Logic Gate Recogniser/Logic Gate RecogniserTests/Logic_Gate_RecogniserTests.swift
@@ -0,0 +1,34 @@
+//
+//  Logic_Gate_RecogniserTests.swift
+//  Logic Gate RecogniserTests
+//
+//  Created by Sonnie Hiles on 16/10/2019.
+//  Copyright © 2019 Sonnie Hiles. All rights reserved.
+//
+
+import XCTest
+@testable import Logic_Gate_Recogniser
+
+class Logic_Gate_RecogniserTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
