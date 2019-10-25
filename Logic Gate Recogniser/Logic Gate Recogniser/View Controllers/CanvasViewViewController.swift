c969fdc0f05b4bd02c4378f7dcab676a26169cdc
Sonnie Hiles <psysh2@nottingham.ac.uk>
2019-10-25 10:11:16 +0100

    Implement basic apple pencil recognition

---
+++ Logic Gate Recogniser/Logic Gate Recogniser/View Controllers/CanvasViewViewController.swift
@@ -0,0 +1,84 @@
+//
+//  CanvasViewViewController.swift
+//  Logic Gate Recogniser
+//
+//  Created by Sonnie Hiles on 25/10/2019.
+//  Copyright © 2019 Sonnie Hiles. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CanvasViewViewController: UIImageView {
+    
+    // Pen Settings
+    private let defaultLineWidth: CGFloat = 10
+    private var drawColor: UIColor = .label
+    
+    // Predictive Drawing
+    private var drawingImage: UIImage?
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+
+        UIGraphicsBeginImageContextWithOptions(bounds.size, false, 0.0)
+        let context = UIGraphicsGetCurrentContext()!
+
+        // Draw previous image into context
+        drawingImage?.draw(in: bounds)
+
+        // Handles extra touches pencil recognises that screen doesn't on real image
+        var touches = [UITouch]()
+        
+        if let coalescedTouches = event?.coalescedTouches(for: touch) {
+            touches = coalescedTouches
+        } else {
+            touches.append(touch)
+        }
+        
+        for touch in touches {
+         self.drawStroke(context: context, touch: touch)
+        }
+        
+        // Draws a dispsoable image using predicted data from apple
+        self.drawingImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        if let predictedTouches = event?.predictedTouches(for: touch) {
+            for touch in predictedTouches {
+                self.drawStroke(context: context, touch: touch)
+            }
+        }
+
+        // Update real image
+        image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        image = drawingImage
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        image = drawingImage
+    }
+    
+    private func drawStroke(context: CGContext, touch: UITouch) {
+        let previousLocation = touch.previousLocation(in: self)
+        let location = touch.location(in: self)
+
+        // Set color
+        drawColor.setStroke()
+
+        // Configure line
+        context.setLineWidth(defaultLineWidth)
+        context.setLineCap(.round)
+
+        // Set up the points
+        context.move(to: CGPoint(x: previousLocation.x, y: previousLocation.y))
+        context.addLine(to: CGPoint(x: location.x, y: location.y))
+        
+        // Draw the stroke
+        context.strokePath()
+     }
+    
+}
--- Logic Gate Recogniser/Logic Gate Recogniser/View Controllers/DrawingViewController.swift
+++ Logic Gate Recogniser/Logic Gate Recogniser/View Controllers/DrawingViewController.swift
@@ -10,65 +10,14 @@ import UIKit
 import PencilKit
 import Combine
 
-class DrawingViewController: UIViewController, PKCanvasViewDelegate, PKToolPickerObserver {
+class DrawingViewController: UIViewController  {
 
-    @IBOutlet weak var canvasView: PKCanvasView!
     @IBOutlet weak var gateInfoView: DetectedGateInfoView!
-    @IBOutlet weak var shareButton: UIBarButtonItem!
-    @IBOutlet weak var saveButton: UIBarButtonItem!
-    
-    let gateClassifier = GateClassifier()
+    @IBOutlet weak var canvasView: CanvasViewViewController!
     
     override func viewWillAppear(_ animated: Bool) {
         // General Setup
         self.title = "Recogniser"
-        
-        // Set up the canvas view
-        canvasView.delegate = self
-        canvasView.alwaysBounceVertical = true
-        canvasView.allowsFingerDrawing = true
-        
-        // Set up the tool picker, using the window of our parent because our view has not been added to a window yet.
-        if let window = parent?.view.window, let toolPicker = PKToolPicker.shared(for: window) {
-            toolPicker.setVisible(true, forFirstResponder: canvasView)
-            toolPicker.addObserver(canvasView)
-            toolPicker.addObserver(self)
-            canvasView.becomeFirstResponder()
-        }
-    }
-    
-    func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
-        let image = getCanvasImage().toBuffer()!
-        let guess = try? gateClassifier.prediction(image: image)
-    
-        guard let g = guess else { return }
-        gateInfoView.recognisedGate.text = g.classLabel
-        gateInfoView.confidence.text = "Confidence: " + String(format: "%.0f", g.classLabelProbs[g.classLabel]! * 100) + "%"
-        print(g.classLabelProbs)
-    }
-    
-    @IBAction func shareButtonTapped(_ sender: Any) {
-        let activityViewController = UIActivityViewController(activityItems: [getCanvasImage()], applicationActivities: nil)
-        activityViewController.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
-        self.present(activityViewController, animated: true, completion: nil)
-    }
-    
-    @IBAction func saveButtonTapped(_ sender: Any) {
-        UIImageWriteToSavedPhotosAlbum(getCanvasImage(), self, nil, nil)
-        canvasView.drawing = PKDrawing()
-        gateInfoView.recognisedGate.text = "Not Gate Recognised"
-        gateInfoView.confidence.text = "Confidence: 100%"
-    }
-    
-    private func getCanvasImage() -> UIImage {
-        var image: UIImage?
-        
-        UITraitCollection(userInterfaceStyle: .light).performAsCurrent {
-            let drawing = canvasView.drawing
-            image = drawing.image(from: drawing.bounds, scale: 1.0)
-        }
-        
-        return (image?.resized()?.addBackground()!)!
     }
 }
 
