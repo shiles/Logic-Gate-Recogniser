e0c9e34e77ec92fe93a198a18008a65f5b66972c
Sonnie Hiles <psysh2@nottingham.ac.uk>
2020-02-13 11:31:08 +0000

    Add draw implementations to gates

--- Logic Gate Recogniser/Logic Gate Recogniser/Gate Model/Gates/Gate.swift
+++ Logic Gate Recogniser/Logic Gate Recogniser/Gate Model/Gates/Gate.swift
@@ -21,46 +21,216 @@ struct Not: Gate {
     var description: String { "Not" }
     var boundingBox: CGRect
     
-    func draw() {}
+    func draw() {
+        let path = UIBezierPath()
+               
+       // Draw Triangle
+       path.move(to: CGPoint(x: 0.1, y: 0.2))
+       path.addLine(to: CGPoint(x: 0.1, y: 0.8))
+       path.addLine(to: CGPoint(x: 0.7, y: 0.5))
+       path.addLine(to: CGPoint(x: 0.1, y: 0.2))
+       
+       // Draw Circle
+       path.move(to: CGPoint(x: 0.9, y: 0.5))
+       path.addArc(withCenter: CGPoint(x: 0.8, y: 0.5), radius: 0.1, startAngle: 0.0, endAngle: .pi*2, clockwise: true)
+       
+       // Draw Input
+       path.move(to: CGPoint(x: 0, y: 0.5))
+       path.addLine(to: CGPoint(x: 0.1, y: 0.5))
+       
+       // Draw Ouput
+       path.move(to: CGPoint(x: 0.9, y: 0.5))
+       path.addLine(to: CGPoint(x: 1, y: 0.5))
+       
+       path.scaleToFit(boundingBox)
+       path.stroke()
+    }
 }
 
 struct Or: Gate {
     var description: String { "Or" }
     var boundingBox: CGRect
-    func draw() {}
+    
+    func draw() {
+        let path = UIBezierPath()
+               
+        // Draw Triangle
+        path.move(to: CGPoint(x: 0.1, y: 0.2))
+        path.addCurve(to: CGPoint(x: 0.1, y: 0.8), controlPoint1: CGPoint(x: 0.25, y: 0.4), controlPoint2: CGPoint(x: 0.25, y: 0.6))
+        path.addCurve(to: CGPoint(x: 0.7, y: 0.5), controlPoint1: CGPoint(x: 0.2, y: 0.85), controlPoint2: CGPoint(x: 0.6, y: 0.6))
+        path.addCurve(to: CGPoint(x: 0.1, y: 0.2), controlPoint1: CGPoint(x: 0.6, y: 0.35), controlPoint2: CGPoint(x: 0.2, y: 0.15))
+        
+        //Draw Inputs
+        path.move(to: CGPoint(x: 0, y: 0.35))
+        path.addLine(to: CGPoint(x: 0.182, y: 0.35))
+        path.move(to: CGPoint(x: 0, y: 0.65))
+        path.addLine(to: CGPoint(x: 0.182, y: 0.65))
+        
+        //Draw Ouput
+        path.move(to: CGPoint(x: 0.7, y: 0.5))
+        path.addLine(to: CGPoint(x: 0.8, y: 0.5))
+        
+        path.scaleToFit(boundingBox)
+        path.stroke()
+    }
 }
 
 struct Nor: Gate {
     var description: String { "Nor" }
     var boundingBox: CGRect
 
-    func draw() {}
+    func draw() {
+        let path = UIBezierPath()
+               
+        // Draw Triangle
+        path.move(to: CGPoint(x: 0.1, y: 0.2))
+        path.addCurve(to: CGPoint(x: 0.1, y: 0.8), controlPoint1: CGPoint(x: 0.25, y: 0.4), controlPoint2: CGPoint(x: 0.25, y: 0.6))
+        path.addCurve(to: CGPoint(x: 0.7, y: 0.5), controlPoint1: CGPoint(x: 0.2, y: 0.85), controlPoint2: CGPoint(x: 0.6, y: 0.6))
+        path.addCurve(to: CGPoint(x: 0.1, y: 0.2), controlPoint1: CGPoint(x: 0.6, y: 0.35), controlPoint2: CGPoint(x: 0.2, y: 0.15))
+        
+        // Draw Circle
+        path.move(to: CGPoint(x: 0.9, y: 0.5))
+        path.addArc(withCenter: CGPoint(x: 0.8, y: 0.5), radius: 0.1, startAngle: 0.0, endAngle: .pi*2, clockwise: true)
+        
+        //Draw Inputs
+        path.move(to: CGPoint(x: 0, y: 0.35))
+        path.addLine(to: CGPoint(x: 0.182, y: 0.35))
+        path.move(to: CGPoint(x: 0, y: 0.65))
+        path.addLine(to: CGPoint(x: 0.182, y: 0.65))
+        
+        // Draw Ouput
+        path.move(to: CGPoint(x: 0.9, y: 0.5))
+        path.addLine(to: CGPoint(x: 1, y: 0.5))
+
+        path.scaleToFit(boundingBox)
+        path.stroke()
+    }
 }
 
 struct And: Gate {
     var description: String { "And" }
     var boundingBox: CGRect
 
-    func draw() {}
+    func draw() {
+        let path = UIBezierPath()
+                      
+        // Draw "Rectangle"
+        path.move(to: CGPoint(x: 0.1, y: 0.2))
+        path.addLine(to: CGPoint(x: 0.1, y: 0.8))
+        path.addLine(to: CGPoint(x: 0.5, y: 0.8))
+        path.addCurve(to: CGPoint(x: 0.5, y: 0.2), controlPoint1: CGPoint(x: 0.77, y: 0.7), controlPoint2: CGPoint(x: 0.77, y: 0.3))
+        path.addLine(to: CGPoint(x: 0.1, y: 0.2))
+
+        //Draw Inputs
+        path.move(to: CGPoint(x: 0, y: 0.35))
+        path.addLine(to: CGPoint(x: 0.1, y: 0.35))
+        path.move(to: CGPoint(x: 0, y: 0.65))
+        path.addLine(to: CGPoint(x: 0.1, y: 0.65))
+
+        // Draw Ouput
+        path.move(to: CGPoint(x: 0.7, y: 0.5))
+        path.addLine(to: CGPoint(x: 0.8, y: 0.5))
+
+        path.scaleToFit(boundingBox)
+        path.stroke()
+    }
 }
 
 struct Nand: Gate {
     var description: String { "Nand" }
     var boundingBox: CGRect
     
-    func draw() {}
+    func draw() {
+        let path = UIBezierPath()
+                      
+        // Draw "Rectangle"
+        path.move(to: CGPoint(x: 0.1, y: 0.2))
+        path.addLine(to: CGPoint(x: 0.1, y: 0.8))
+        path.addLine(to: CGPoint(x: 0.5, y: 0.8))
+        path.addCurve(to: CGPoint(x: 0.5, y: 0.2), controlPoint1: CGPoint(x: 0.77, y: 0.7), controlPoint2: CGPoint(x: 0.77, y: 0.3))
+        path.addLine(to: CGPoint(x: 0.1, y: 0.2))
+
+        // Draw Circle
+        path.move(to: CGPoint(x: 0.9, y: 0.5))
+        path.addArc(withCenter: CGPoint(x: 0.8, y: 0.5), radius: 0.1, startAngle: 0.0, endAngle: .pi*2, clockwise: true)
+        
+        //Draw Inputs
+        path.move(to: CGPoint(x: 0, y: 0.35))
+        path.addLine(to: CGPoint(x: 0.1, y: 0.35))
+        path.move(to: CGPoint(x: 0, y: 0.65))
+        path.addLine(to: CGPoint(x: 0.1, y: 0.65))
+
+        // Draw Ouput
+        path.move(to: CGPoint(x: 0.9, y: 0.5))
+        path.addLine(to: CGPoint(x: 1, y: 0.5))
+
+        path.scaleToFit(boundingBox)
+        path.stroke()
+    }
 }
 
 struct Xor: Gate {
     var description: String { "Xor" }
     var boundingBox: CGRect
    
-   func draw() {}
+    func draw() {
+        let path = UIBezierPath()
+        
+        // Draw First Line
+        path.move(to: CGPoint(x: 0.1, y: 0.2))
+        path.addCurve(to: CGPoint(x: 0.1, y: 0.8), controlPoint1: CGPoint(x: 0.25, y: 0.4), controlPoint2: CGPoint(x: 0.25, y: 0.6))
+        
+        path.move(to: CGPoint(x: 0.15, y: 0.2))
+        path.addCurve(to: CGPoint(x: 0.15, y: 0.8), controlPoint1: CGPoint(x: 0.3, y: 0.4), controlPoint2: CGPoint(x: 0.3, y: 0.6))
+        path.addCurve(to: CGPoint(x: 0.7, y: 0.5), controlPoint1: CGPoint(x: 0.2, y: 0.85), controlPoint2: CGPoint(x: 0.6, y: 0.6))
+        path.addCurve(to: CGPoint(x: 0.15, y: 0.2), controlPoint1: CGPoint(x: 0.6, y: 0.35), controlPoint2: CGPoint(x: 0.2, y: 0.15))
+        
+        //Draw Inputs
+        path.move(to: CGPoint(x: 0, y: 0.35))
+        path.addLine(to: CGPoint(x: 0.182, y: 0.35))
+        path.move(to: CGPoint(x: 0, y: 0.65))
+        path.addLine(to: CGPoint(x: 0.182, y: 0.65))
+        
+        //Draw Ouput
+        path.move(to: CGPoint(x: 0.7, y: 0.5))
+        path.addLine(to: CGPoint(x: 0.8, y: 0.5))
+        
+        path.scaleToFit(boundingBox)
+        path.stroke()
+    }
 }
 
 struct Xnor: Gate {
     var description: String { "Xnor" }
     var boundingBox: CGRect
    
-    func draw() {}
+    func draw() {
+        let path = UIBezierPath()
+               
+       // Draw First Line
+       path.move(to: CGPoint(x: 0.1, y: 0.2))
+       path.addCurve(to: CGPoint(x: 0.1, y: 0.8), controlPoint1: CGPoint(x: 0.25, y: 0.4), controlPoint2: CGPoint(x: 0.25, y: 0.6))
+       
+       path.move(to: CGPoint(x: 0.15, y: 0.2))
+       path.addCurve(to: CGPoint(x: 0.15, y: 0.8), controlPoint1: CGPoint(x: 0.3, y: 0.4), controlPoint2: CGPoint(x: 0.3, y: 0.6))
+       path.addCurve(to: CGPoint(x: 0.7, y: 0.5), controlPoint1: CGPoint(x: 0.2, y: 0.85), controlPoint2: CGPoint(x: 0.6, y: 0.6))
+       path.addCurve(to: CGPoint(x: 0.15, y: 0.2), controlPoint1: CGPoint(x: 0.6, y: 0.35), controlPoint2: CGPoint(x: 0.2, y: 0.15))
+       
+       // Draw Circle
+       path.move(to: CGPoint(x: 0.9, y: 0.5))
+       path.addArc(withCenter: CGPoint(x: 0.8, y: 0.5), radius: 0.1, startAngle: 0.0, endAngle: .pi*2, clockwise: true)
+              
+       //Draw Inputs
+       path.move(to: CGPoint(x: 0, y: 0.35))
+       path.addLine(to: CGPoint(x: 0.182, y: 0.35))
+       path.move(to: CGPoint(x: 0, y: 0.65))
+       path.addLine(to: CGPoint(x: 0.182, y: 0.65))
+       
+       // Draw Ouput
+       path.move(to: CGPoint(x: 0.9, y: 0.5))
+       path.addLine(to: CGPoint(x: 1, y: 0.5))
+              
+       path.scaleToFit(boundingBox)
+       path.stroke()
+    }
 }
