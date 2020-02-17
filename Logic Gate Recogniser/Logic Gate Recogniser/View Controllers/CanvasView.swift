c96c23b4505a01ec969ee5f1fa696142943efa4b
Sonnie Hiles <psysh2@nottingham.ac.uk>
2020-02-17 12:42:49 +0000

    Remove debug drawing code for shapes

--- Logic Gate Recogniser/Logic Gate Recogniser/View Controllers/CanvasView.swift
+++ Logic Gate Recogniser/Logic Gate Recogniser/View Controllers/CanvasView.swift
@@ -27,7 +27,7 @@ class CanvasView: UIImageView {
                     defaultLineWidth = 10
                 case .erasor:
                     drawColor = .systemBackground
-                    defaultLineWidth = 30
+                    defaultLineWidth = 20
                 case .none: fatalError()
             }
         }
@@ -81,25 +81,9 @@ class CanvasView: UIImageView {
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         image = drawingImage
     
-//        guard let hull = recogniser.convexHull(of: points) else { points = []; return}
-//        drawConvexHull(convexHull: hull)
-//
-//        guard let triangle = recogniser.largestAreaTriangle(using: hull) else { points = []; return}
-//        drawTriangle(triangle: triangle)
-//
-//        let minRect = recogniser.boundingBox(using: hull)
-//        drawCorners(boundingBox: minRect)
-        
         drawingRecogniser.recogniseShape(from: points)
         strokes.append(points)
         points = []
-        
-//        drawingRecogniser.adjacentShapes.flatMap({ $0 }).forEach {
-//            let box = $0.inflatedBoundingBox
-//            let list = [CGPoint(x: box.minX, y: box.minY), CGPoint(x: box.minX, y: box.maxY),
-//                        CGPoint(x: box.maxX, y: box.maxY), CGPoint(x: box.maxX, y: box.minY)]
-//            drawConvexHull(convexHull: list, colour: .blue)
-//        }
         
         analysisTimer = Timer.scheduledTimer(
             timeInterval: 1,
@@ -159,117 +143,5 @@ class CanvasView: UIImageView {
         context.move(to: stroke.first!)
         (1...stroke.lastIndex).forEach { context.addLine(to: stroke[$0]) }
         context.strokePath()
-    }
-    
-    // MARK: Debug Drawing Methods
-    
-    func drawRecognisedLine(line: Line, colour: UIColor = UIColor(hue: drand48())) {
-        UIGraphicsBeginImageContextWithOptions(bounds.size, false, 0.0)
-        let context = UIGraphicsGetCurrentContext()!
-
-        // Draw previous image into context
-        drawingImage?.draw(in: bounds)
-        
-        // Line setup
-        context.setLineCap(.round)
-        context.setLineWidth(6.0)
-        context.setStrokeColor(colour.cgColor)
-
-        // Add lines
-        context.move(to: line.startPoint)
-        context.addLine(to: line.endPoint)
-
-        // Draw line
-        context.strokePath()
-
-        // Update real image
-        drawingImage = UIGraphicsGetImageFromCurrentImageContext()
-        image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-    }
-    
-    func drawConvexHull(convexHull: ConvexHull, colour: UIColor = UIColor.red) {
-        if convexHull.isEmpty { return }
-        
-        UIGraphicsBeginImageContextWithOptions(bounds.size, false, 0.0)
-        let context = UIGraphicsGetCurrentContext()!
-
-        // Draw previous image into context
-        drawingImage?.draw(in: bounds)
-        
-        // Line setup
-        context.setLineCap(.round)
-        context.setLineWidth(6.0)
-        context.setStrokeColor(colour.cgColor)
-
-        // Add lines
-        context.move(to: convexHull.first!)
-        convexHull.forEach { context.addLine(to: $0) }
-        context.addLine(to: convexHull.first!)
-        
-        // Draw line
-        context.strokePath()
-
-        // Update real image
-        drawingImage = UIGraphicsGetImageFromCurrentImageContext()
-        image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-    }
-    
-    func drawTriangle(triangle: Triangle, colour: UIColor = UIColor.blue) {        
-        UIGraphicsBeginImageContextWithOptions(bounds.size, false, 0.0)
-        let context = UIGraphicsGetCurrentContext()!
-
-        // Draw previous image into context
-        drawingImage?.draw(in: bounds)
-        
-        // Line setup
-        context.setLineCap(.round)
-        context.setLineWidth(6.0)
-        context.setStrokeColor(colour.cgColor)
-
-        // Add lines
-        context.move(to: triangle.a)
-        context.addLine(to: triangle.b)
-        context.addLine(to: triangle.c)
-        context.addLine(to: triangle.a)
-        
-        // Draw line
-        context.strokePath()
-
-        // Update real image
-        drawingImage = UIGraphicsGetImageFromCurrentImageContext()
-        image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-    }
-    
-    func drawCorners(boundingBox: BoundingBox, colour: UIColor = UIColor.green) {
-        UIGraphicsBeginImageContextWithOptions(bounds.size, false, 0.0)
-        let context = UIGraphicsGetCurrentContext()!
-
-        // Draw previous image into context
-        drawingImage?.draw(in: bounds)
-        
-        // Line setup
-        context.setLineCap(.round)
-        context.setLineWidth(6.0)
-        context.setStrokeColor(colour.cgColor)
-
-        let cornerPoints = boundingBox.cornerPoints
-        
-        // Add lines
-        context.move(to: cornerPoints.p1)
-        context.addLine(to: cornerPoints.p2)
-        context.addLine(to: cornerPoints.p3)
-        context.addLine(to: cornerPoints.p4)
-        context.addLine(to: cornerPoints.p1)
-        
-        // Draw line
-        context.strokePath()
-
-        // Update real image
-        drawingImage = UIGraphicsGetImageFromCurrentImageContext()
-        image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
     }
 }
